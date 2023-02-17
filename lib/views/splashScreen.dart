ad3adf05490ef8703fbbef105b16bfdab0d0c918
Abhay Tomar <75438874+abhay-tomar03@users.noreply.github.com>
2023-02-18 00:27:44 +0530

    Add files via upload

---
+++ lib/views/splashScreen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+import 'home_page.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  SplashScreenState createState() => SplashScreenState();
+}
+
+class SplashScreenState extends State<SplashScreen> with TickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          Lottie.asset(
+            'assets/developer-yoga.json',
+            controller: _controller,
+            onLoaded: (composition) {
+              _controller
+                ..duration = composition.duration
+                ..forward().whenComplete(() => Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const HomePage()),
+                    ));
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
